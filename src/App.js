30e1199f5e2bbe861902cca4335de52fef0dbd15
arijitkar <arijitkar29@gmail.com>
2023-03-25 22:00:14 +0530

    better

--- src/App.js
+++ src/App.js
@@ -4,73 +4,66 @@ import Alert from "./components/Alert";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 
-
-
 function App() {
-
-  const [moder, setmoder] = useState("light")
-  const [alert, setalert] = useState(null)
+  const [moder, setmoder] = useState("light");
+  const [alert, setalert] = useState(null);
 
   const showAlert = (message, type) => {
-    setalert(
-      {
-        msg: message,
-        type: type,
-        value: 9
-      }
-    )
-
+    setalert({
+      msg: message,
+      type: type,
+      value: 9,
+    });
 
     setTimeout(() => {
-      setalert(null)
+      setalert(null);
     }, 1800);
-  }
-
+  };
 
   const modetoggler = () => {
-    if (moder === 'light') {
-      setmoder('dark');
-      document.body.style.backgroundColor = 'blue';
+    if (moder === "light") {
+      setmoder("dark");
+      document.body.style.backgroundColor = "blue";
 
-      showAlert("dark mode enabled", "success")
-
+      showAlert("dark mode enabled", "success");
+    } else {
+      setmoder("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("light mode enabled", "success");
     }
-    else {
-      setmoder('light');
-      document.body.style.backgroundColor = 'white';
-      showAlert("light mode enabled", "success")
-
-
-
-    }
-  }
+  };
   // modetoggler();
 
   return (
-
-
     <div>
-      < Navbar title="Textutils" aboutText="About Textutils" mode={moder} toggle={modetoggler} />
+      <Navbar
+        title="Textutils"
+        aboutText="About Textutils"
+        mode={moder}
+        toggle={modetoggler}
+      />
       <Alert info={alert} />
       <div className="container my-3">
         <Routes>
-          <Route exact  path='/' element={<TextForm heading="Enter your text here" mode={moder} alertmaker={showAlert} />} />
-          <Route exact path='/about' element={<About mode={moder}/>} />
+          <Route
+            exact
+            path="/"
+            element={
+              <TextForm
+                heading="Enter your text here"
+                mode={moder}
+                alertmaker={showAlert}
+              />
+            }
+          />
+          <Route exact path="/about" element={<About mode={moder} />} />
         </Routes>
       </div>
     </div>
-
   );
 }
 
 export default App;
-
-
-
-
-
-
-
